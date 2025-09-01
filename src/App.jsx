1f17d600bed80cf7e7da43c9b2d8bbe114669183
Arkadip <arkadip671@gmail.com>
2025-09-02 00:15:56 +0530

    finalised

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Header from "./sections/Header";
+import Hero from "./sections/Hero";
+import Features from "./sections/Features";
+import Pricing from "./sections/Pricing";
+import Faq from "./sections/Faq";
+import Footer from "./sections/Footer";
+import Testimonials from "./sections/Testimonials";
+
+const App = () => {
+  return (
+    <main className="overflow-hidden">
+      <Header />
+      <Hero />
+      <Features />
+      <Pricing />
+      <Faq />
+      <Testimonials />
+      <Footer />
+    </main>
+  );
+};
+
+export default App;
